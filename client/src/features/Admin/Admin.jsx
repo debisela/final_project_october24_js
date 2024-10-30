ae8865f9e51784d6f6b00d96dd0a8ccd4e5cf7ab
debisela <debora.jaeckel@gmail.com>
2024-10-30 20:23:06 +0200

    admin component

--- client/src/features/Admin/Admin.jsx
+++ client/src/features/Admin/Admin.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useDispatch } from "react-redux";
+import { fetchFields } from "./state/adminSlice";
+import { useFieldsSelector, useFieldsStatus } from "./state/hooks";
 
 const Admin = ()=>{
+    const fields = useFieldsSelector()
+    const status = useFieldsStatus()
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+        dispatch(fetchFields())
+    }, [dispatch])
 
 
+    if (status === 'loading') return <h2>Loading...</h2>
+    if (status === 'failed') return <h2>Can't get fields...</h2>
     return(
         <>
-        <h2>Admin</h2>
+        <h2>Fields Selection</h2>
+        <div>
+            {fields.map((item, index)=>(
+                <div key={index}>
+                    <input
+                    
+                    
+                    
+                    
+                    />
+                </div>
+            ))}
+        </div>
         </>
     )
 }
