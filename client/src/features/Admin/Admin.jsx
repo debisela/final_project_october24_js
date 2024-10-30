cb02dd149ba7abe2d2090bf4cb8946ff9e58858e
debisela <debora.jaeckel@gmail.com>
2024-10-30 21:14:08 +0200

    save selected fields in db and show them

--- client/src/features/Admin/Admin.jsx
+++ client/src/features/Admin/Admin.jsx
@@ -1,16 +1,32 @@
 import React, {useEffect} from "react";
 import { useDispatch } from "react-redux";
-import { fetchFields } from "./state/adminSlice";
-import { useFieldsSelector, useFieldsStatus } from "./state/hooks";
+import { fetchFields, fetchSelectedFields, selectField, toggleFieldSelection } from "./state/adminSlice";
+import { useFieldsSelector, useFieldsSelection, useFieldsStatus } from "./state/hooks";
 
 const Admin = ()=>{
     const fields = useFieldsSelector()
+    const selectedFields = useFieldsSelection()
     const status = useFieldsStatus()
     const dispatch = useDispatch()
 
     useEffect(()=>{
         dispatch(fetchFields())
+        dispatch(fetchSelectedFields())
     }, [dispatch])
+    console.log('selected fields', selectedFields);
+    
+
+    // Dispatch an action to toggle field selection
+  const handleFieldChange = (e) => {
+    const {value} = e.target;
+    dispatch(toggleFieldSelection(value)); // Toggle field selection in the Redux store
+  };
+
+    // Handle form submission (saving selected fields)
+  const handleSubmit = () => {
+    console.log("Selected fields to save:", selectedFields);
+    dispatch(selectField(selectedFields)); // Dispatch action to save selected fields
+  };
 
 
     if (status === 'loading') return <h2>Loading...</h2>
@@ -24,14 +40,13 @@ const Admin = ()=>{
                     <input
                     type="checkbox"
                     value={item.column_name}
-                    
-                    
-                    
-                    
+                    checked={selectedFields.includes(item.column_name)}
+                    onChange={handleFieldChange}
                     />
                     <label>{item.column_name}</label>
                 </div>
             ))}
+           <button onClick={handleSubmit}>Save my fields</button>
         </div>
         </>
     )
