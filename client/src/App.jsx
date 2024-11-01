0245e47271cf3f2185388fc7f88c006e0907f014
debisela <debora.jaeckel@gmail.com>
2024-11-01 17:06:37 +0200

    added header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes, useLocation } from 'react-router-dom'
 import IntroPage from './features/Welcome/IntroPage'
 import Admin from './features/Admin/Admin'
 import User from './features/User/User'
+import Header from './features/Header/Header'
 
 function App() {
   const location = useLocation()
@@ -9,6 +10,7 @@ function App() {
 
   return (
     <>
+      {location.pathname !=='/' && <Header/>}
      <Routes>
      <Route path="/" element={<IntroPage/>}/>
       <Route path="/user" element={<User/>}/>
